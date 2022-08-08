20a406571c6cb4d310b77534579e77dc11bec1e7
jcui65 <cuijn@seas.upenn.edu>
2022-08-07 20:41:32 -0400

    This is my 10th major commit on LS3!

--- latentsafesets/rl_trainers/cbfdot_trainer.py
+++ latentsafesets/rl_trainers/cbfdot_trainer.py
@@ -16,7 +16,7 @@ import os
 log = logging.getLogger("cbfd train")
 
 
-class CBFdotTrainer(Trainer):
+class CBFdotTrainer(Trainer):#modified from contraint trainer
     def __init__(self, env, params, cbfd, loss_plotter):
         self.params = params
         self.cbfd =cbfd
@@ -35,16 +35,17 @@ class CBFdotTrainer(Trainer):
         for i in range(self.params['cbfd_init_iters']):#10000
             out_dict = replay_buffer.sample(self.params['cbfd_batch_size'])#256
             rdo, action, hvd = out_dict['rdo'], out_dict['action'], out_dict['hvd']#0 or 1
+            #rdo for relative distance old, hvd for h value difference
             #print('rdo',rdo)#seems reasonable##print(rdo.shape)#(256,2)
             #print('action',action)#seems reasonable##print(action.shape)#(256,2)
-            rda=np.concatenate((rdo,action),axis=1)
+            rda=np.concatenate((rdo,action),axis=1)#rda for relative distance+action
             #print(rda.shape)#(256,4)
             #print('hvd',hvd)#seems reasonable##print(hvd.shape)#(256,)
             #rdn,hvo,hvn=out_dict['rdn'], out_dict['hvo'], out_dict['hvn']#
             #print('rdn',rdn)#seems reasonable#
             #print('hvo',hvo)#seems reasonable#
             #print('hvn',hvn)#seems reasonable#
-            loss, info = self.cbfd.update(rda, hvd, already_embedded=True)
+            loss, info = self.cbfd.update(rda, hvd, already_embedded=True)#not the update below
             self.loss_plotter.add_data(info)#self.constr.update, not self.update!
 
             if i % self.params['log_freq'] == 0:
@@ -58,7 +59,7 @@ class CBFdotTrainer(Trainer):
 
         self.cbfd.save(os.path.join(update_dir, 'cbfd.pth'))
 
-    def update(self, replay_buffer, update_dir):
+    def update(self, replay_buffer, update_dir):#this is the update process after initial train
         log.info('Beginning cbf dott update optimization')
 
         for _ in trange(self.params['cbfd_update_iters']):
